32834a8712266dff8e4337a0f62d052de5067abf
blendonluta <blendonluta98@gmail.com>
2023-01-10 17:44:50 +0100

    initial commit

---
+++ src/main/java/com/pek/ttlivescoreapi/TtLiveScoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.pek.ttlivescoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtLiveScoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtLiveScoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pek/ttlivescoreapi/TtLiveScoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pek.ttlivescoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtLiveScoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
