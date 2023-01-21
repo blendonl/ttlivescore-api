2f8afba7c1e0263224ea350332832bc3b777478a
blendonluta <blendonluta98@gmail.com>
2023-01-21 09:08:32 +0100

    change response to dtos

---
+++ src/main/java/com/pek/ttlivescoreapi/dto/UserDto.java
@@ -0,0 +1,23 @@
+package com.pek.ttlivescoreapi.dto;
+
+import com.pek.ttlivescoreapi.entity.User;
+import lombok.*;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private Date birthDate;
+    private char gender;
+
+
+
+}
---
+++ src/main/java/com/pek/ttlivescoreapi/mapper/UserDtoMapper.java
@@ -0,0 +1,21 @@
+package com.pek.ttlivescoreapi.mapper;
+
+import com.pek.ttlivescoreapi.dto.UserDto;
+import com.pek.ttlivescoreapi.entity.User;
+
+import java.util.List;
+
+public class UserDtoMapper {
+
+    public UserDto mapUserToUserDto(User user) {
+         return UserDto.builder()
+                .name(user.getName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .birthDate(user.getBirthDate())
+                .gender(user.getGender())
+                .build();
+    }
+
+
+}
