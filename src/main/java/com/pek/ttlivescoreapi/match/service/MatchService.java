c694805965d4a8a27ced02e953b9e9ba0f30645b
blendonluta <blendonluta@gmail.com>
2023-03-09 09:41:11 +0100

    fix service bean not added

--- src/main/java/com/pek/ttlivescoreapi/match/service/MatchService.java
+++ src/main/java/com/pek/ttlivescoreapi/match/service/MatchService.java
@@ -2,8 +2,10 @@ package com.pek.ttlivescoreapi.match.service;
 
 import com.pek.ttlivescoreapi.user.entity.User;
 import com.pek.ttlivescoreapi.user.transport.UserTransport;
+import org.springframework.stereotype.Service;
 
 
+@Service
 public interface MatchService {
     UserTransport getWinnerInASingleMatch(long singleMatchId);
 
