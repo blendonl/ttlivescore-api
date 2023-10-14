732dd21ef859e47573f99f3b01186888a8a52671
blendonl <blendonluta@gmail.com>
2023-10-14 11:39:03 +0200

    fix cors bug with new version

--- src/main/java/com/pek/ttlivescoreapi/TtLiveScoreApiApplication.java
+++ src/main/java/com/pek/ttlivescoreapi/TtLiveScoreApiApplication.java
@@ -1,8 +1,12 @@
 package com.pek.ttlivescoreapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class TtLiveScoreApiApplication {
 
---
+++ src/main/java/com/pek/ttlivescoreapi/config/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.pek.ttlivescoreapi.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+         http
+             .cors(Customizer.withDefaults())
+             .authorizeHttpRequests(auth -> auth
+                     .requestMatchers("**").permitAll()
+                     .requestMatchers("/users/**", "/users/players/**")
+                     .permitAll()
+                     .requestMatchers("/match/**", "/match/players/**")
+                     .permitAll()
+                     .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+
+}
