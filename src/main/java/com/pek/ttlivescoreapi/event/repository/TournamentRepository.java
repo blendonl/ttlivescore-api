715e6efce22df4f41cf5418765f77eaa36045a50
blendonl <blendonluta@gmail.com>
2024-01-12 18:22:22 +0100

    fix category bug for tournament create

--- src/main/java/com/pek/ttlivescoreapi/event/repository/TournamentRepository.java
+++ src/main/java/com/pek/ttlivescoreapi/event/repository/TournamentRepository.java
@@ -15,6 +15,6 @@ public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     @Query(value = "select * from tournament t left join event e on t.event_id = e.id where date_part('year', date) = :#{#query.year} or gender = :#{#query.gender}", nativeQuery = true)
     List<Tournament> findAllByQuery(@Param("query") TournamentQueryTransport query);
 
-    @Query(value = "select case when count(*) = 1 then True else False end from event e left join tournament t on e.id = t.event_id left join category c on c.id = t.category_id where e.name = :name and c.name = :category", nativeQuery = true)
+    @Query(value = "select case when count(*) = 1 then True else False end from event e left join tournament t on e.id = t.event_id where e.name = :name and category = :category", nativeQuery = true)
     boolean existsByNameAndCategoryName(String name, String category);
 }
